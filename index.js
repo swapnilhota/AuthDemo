92c34bc4055710378a10316db24445cc9e4da663
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-04 10:45:23 +0530

    Created Register Page

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const User = require('./models/user');
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
 app.get('/', (req, res) => {
     res.send(`
         <h1>Home Page</h1>
         <a href="/secret">Want to see a Secret?</a>
+        <a href="/register">Register</a>
     `)
 })
 
