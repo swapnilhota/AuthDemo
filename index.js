c34b6ab0cafbc6b47f4e34b5202be1e0cc1f6bf4
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-04 10:57:00 +0530

    setting up POST route

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+    .then(() => {
+        console.log("Database Connected");
+    })
+    .catch(err => {
+        console.log("Error Occurred");
+        console.log(err);
+    })
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/register', (req, res) => {
     res.render('register');
+})
+
+app.post('/register', async (req, res) => {
+
 })
 
 app.get('/', (req, res) => {
